6dada8b6bea414492638ff363307d25238deb205
ZoiaAkramJatt <zoiaakramjat11zz@gmail.com>
2025-07-08 10:47:27 -0700

    Added file= while loop in Pyhton

---
+++ while_loop.py
@@ -0,0 +1,42 @@
+#counting 20 time by using while loop
+i=1
+while(i<21):
+    print(i)
+    i += 1
+    
+   #print  zoia five time 
+i=1
+while(i<6):
+    print("zoia")
+    i += 1
+    
+#write table of 2 
+'''n=int(input("Enter the number:"))
+i=1
+while(i<=10):
+    print(f"{n} * {i} = {i*n}")
+    i += 1
+    '''
+
+list=["zoia", "sidra",99]
+i=0
+while(i<5):
+    print(list)
+    i += 1
+    
+#table 
+z=int(input("Enter the number"))
+i=1
+while(i<11):
+    print(f"{z} * {i} = {z*i}")
+    i+=1
+    
+    
+    
+
+    
+
+    
+
+    
+    
